bf0fe30a924a5aaf3c5c984a80ab8cc015954d4a
Sahil Gore <162705835+SahilGore83@users.noreply.github.com>
2025-05-16 20:58:11 +0000

    Strings

---
+++ 01_Basics/String.js
@@ -0,0 +1,27 @@
+const name = "Sahil"
+const repoCount = 1
+
+// console.log(name + repoCount + "value")
+
+// console.log(`Hi my name is ${name} my repocount is ${repoCount}`);
+
+const Sahil = new String('Sahil-SG-Gore')
+
+
+console.log(Sahil.charAt(2));
+console.log(Sahil.indexOf('i'));
+console.log(Sahil.substring(0, 2));
+console.log(Sahil.length);
+
+const newstring =  Sahil.slice()
+console.log(newstring);
+
+const Anotherstring = "    Sahil    "
+console.log(Anotherstring);
+console.log(Anotherstring.trim());
+
+const url = "https://Sahil.com/gore@3sahil"
+console.log(url.replace('@3','-'));
+
+console.log(Sahil.split('-'));
+
