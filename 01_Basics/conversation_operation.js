108a52d9da53080144a75fe0fb0366a149dd109b
Sahil Gore <162705835+SahilGore83@users.noreply.github.com>
2025-05-16 20:00:47 +0000

    Conversation Operation done

---
+++ 01_Basics/conversation_operation.js
@@ -0,0 +1,32 @@
+let score ="Sahil"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// *******************operations************************
+
+ let value = 3 
+ let negvalue = -value
+//  console.log(negvalue);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+// console.log(2**4);
+
+let str1 ="hello"
+let str2 = "Sahil"
+
+let str3 = str1 + str2
+console.log(str3);
+ 
