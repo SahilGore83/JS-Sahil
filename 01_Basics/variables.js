775241dcd21ff00424ba74ff2f3f76f6d0639141
Sahil Gore <162705835+SahilGore83@users.noreply.github.com>
2025-05-16 19:59:50 +0000

    variables done

---
+++ 01_Basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 12345//const variable
+let accountEmail = "sahil122@gmail.com"//use this insted of var
+var accountPassword = "5454" //variable but now a days no one ues it
+accountCity = "Goa"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
