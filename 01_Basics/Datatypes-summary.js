e3fb5148d1e8bb9717b8fd54837f51cec5fc59fd
Sahil Gore <162705835+SahilGore83@users.noreply.github.com>
2025-05-16 20:07:21 +0000

    Datatype-summary

---
+++ 01_Basics/Datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types :String,Number, Boolean, null ,undefiend, Symbol,BigInt
+
+
+
+
+//Reference (Non Primitive)
+
+//Arry, object , function
+
+const heros = ["Deadpool","Ironaman","naagraj"]//array
+
+let myobj = {
+    name :"sahil",
+    age:20,
+
+}//object
+// console.log(heros);
+// console.log(myobj);
+
+const myfunction = function(){
+    // console.log("Hello World")
+
+}
+console.log(typeof heros);
+
+
+/* 
+Typeof val 
+Undefined = "undefiend"
+Null = "Object"
+Boolean = "Boolean"
+Number = "number"
+String = "string"
+object(native and does implement ([call]) = "object"
+object(native or host and does not implement ([call]) = "Function"
+*/
