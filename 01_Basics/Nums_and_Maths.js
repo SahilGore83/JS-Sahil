c0a2d7f7fff4ba178bb082ba00091efa3f832bd3
Sahil Gore <162705835+SahilGore83@users.noreply.github.com>
2025-05-17 10:31:53 +0000

    Math and Num

---
+++ 01_Basics/Nums_and_Maths.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score);
+
+const price = new Number(100)
+// console.log(price);
+
+// console.log(price.toString().length);
+// console.log(price.toFixed(2));
+
+const anotherNumber = 1129.778
+
+// console.log(anotherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+/* ++++++++++++++++++++++ Maths +++++++++++++++++++++ */
+
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(7.9));
+// console.log(Math.ceil(7.1));
+// console.log(Math.floor(7.9));
+// console.log(Math.min(4,56,6,34,59));
+// console.log(Math.max(4,56,6,34,59));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random()* (max - min +1)) + min);
+
+
