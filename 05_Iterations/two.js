39ade8b5011b83e0d1779d6863335195a7c33c91
Sahil Gore <162705835+SahilGore83@users.noreply.github.com>
2025-05-21 18:56:29 +0000

    while loop understanding

---
+++ 05_Iterations/two.js
@@ -0,0 +1,24 @@
+// while loop
+// let i = 0
+// while (i <=10) {
+//     console.log(` Value of index is ${i}`);
+//     i += 2
+
+// }
+
+// let myArray= [
+//     "Flash", "Iron-man", "Superman","antman"
+// ]
+
+// let arr=0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr +1
+// }
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
