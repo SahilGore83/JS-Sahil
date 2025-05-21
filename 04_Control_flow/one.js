3191a1b8bcfed8255ec5f452a1f209ad62388c25
Sahil Gore <162705835+SahilGore83@users.noreply.github.com>
2025-05-21 17:54:49 +0000

    basics of control flow

--- 04_Control_flow/one.js
+++ 04_Control_flow/one.js
@@ -1,1 +1,55 @@
+// if statement
 
+// if (2 == "2") {
+//     console.log("HI");
+    
+// }
+
+// // < , > , <= , >= , !=, ==, ===, !==
+// if (2 === "2") {
+//     console.log("HI");
+    
+// }
+// else{
+//     console.log("Not equal");
+    
+// }
+
+// const score = 200
+// if(score>100){
+//     let power = "Fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+//console.log(`user power: ${power}`);
+
+
+const balance = 1000
+
+// if (balance > 500) console.log("yeah"),console.log("yeah2");
+
+//  if (balance < 500){
+//     console.log("less than 500");
+    
+//  }else if(balance < 750){
+//     console.log("less than 750");
+    
+//  }else{
+//     console.log("greater than 750");
+    
+//  }
+
+const userloggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromemail = true
+
+if (userloggedIn && debitcard) {
+    console.log("Allow to buy course");
+    
+}
+
+if (loggedInFromGoogle || loggedInFromemail) {
+    console.log("Allow to login in  course");
+    
+}
